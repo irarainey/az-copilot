539ecafee955adf43822e2d22e0a5b389f68da09
irarainey <irarainey@microsoft.com>
2023-10-26 11:13:07 +0100

    Basic working version

--- src/copilot/azext_copilot/__init__.py
+++ src/copilot/azext_copilot/__init__.py
@@ -2,8 +2,8 @@ from azext_copilot.copilot import copilot
 from azure.cli.core import AzCommandsLoader
 
 
-def call_cli():
-    copilot('list my resource groups in a table')
+def call_cli(cmd_prompt):
+    copilot(cmd_prompt)
 
 
 class CopilotCommandsLoader(AzCommandsLoader):
@@ -14,16 +14,15 @@ class CopilotCommandsLoader(AzCommandsLoader):
         super(CopilotCommandsLoader, self).__init__(cli_ctx=cli_ctx,
                                                     custom_command_type=custom_type)
 
-    def load_command_table(self, args):
-
-        with self.command_group('copilot') as g:
-            g.custom_command('tips', 'call_cli')
+    def load_command_table(self, _):
+        with self.command_group('') as g:
+            g.custom_command('copilot', 'call_cli')
 
         return self.command_table
 
     def load_arguments(self, _):
-        # No arguments to load
-        pass
+        with self.argument_context('copilot') as c:
+            c.argument('cmd_prompt', options_list=['--command', '-c'], help='The plain English command you wish to execute.')
 
 
 COMMAND_LOADER_CLS = CopilotCommandsLoader
